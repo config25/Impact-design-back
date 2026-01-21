15d99d6b06fa2146d905021eaddf1a3e5285d4ae
config25 <yhkim052556@naver.com>
2026-01-21 14:40:41 +0900

    Initial Spring Boot project structure

---
+++ src/test/java/qtedu/Impact_design/ImpactDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package qtedu.Impact_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
